a2343001b121f5f635e00af4dc90e6b4f5a798df
MoKenawy <moken@LAPTOP-IH3NAIAC>
2024-09-20 16:19:37 +0300

    .

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -13,7 +13,7 @@ function Header({ links = [] }: { links?: NavLink[] }) {
         <Navbar className="flex w-full" maxWidth="full">
             <NavbarBrand>
                 
-                <Image src="/logo.jpg" className="h-12 w-auto rounded-full" alt="logo" width={50} height={50} />
+                <Image src="logo.jpg" className="h-12 w-auto rounded-full" alt="logo" width={50} height={50} />
                 <p className="font-bold text-inherit ml-2">Mohammed Kenawy</p>
             </NavbarBrand>
 

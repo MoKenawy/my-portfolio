b08fca64e11e5e042f9efbb51a0e0cfb44070da0
MoKenawy <moken@LAPTOP-IH3NAIAC>
2024-09-20 14:07:50 +0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/",
+    output: "export",
+    reactStrictMode: true,
+  };
+module.exports = nextConfig;
 
 export default nextConfig;

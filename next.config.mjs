14c3257915eef7d43c8487b5743a8f31966dee65
MoKenawy <moken@LAPTOP-IH3NAIAC>
2024-09-20 15:05:27 +0300

    Bug Fix: next.config not found

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/",
     output: "export",
     reactStrictMode: true,
   };
-module.exports = nextConfig;
 
 export default nextConfig;
